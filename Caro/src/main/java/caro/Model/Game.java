950d672d5a95f2cf82ce43132bd9c81667a2536d
12.24 <76689037+tinhyeuol1205@users.noreply.github.com>
2022-11-10 18:03:35 +0700

    Update Game.java

--- Caro/src/main/java/caro/Model/Game.java
+++ Caro/src/main/java/caro/Model/Game.java
@@ -5,16 +5,68 @@ public class Game {
     private User user;
     private int turn = 0;
     private long time;
+    private int depth = 3;
     private int winner = 0;
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public Minimax getAI() {
+        return AI;
+    }
+
+    public void setAI(Minimax AI) {
+        this.AI = AI;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public void setTurn(int turn) {
+        this.turn = turn;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public int getWinner() {
+        return winner;
+    }
+
+    public void setWinner(int winner) {
+        this.winner = winner;
+    }
 
     public Game(Board board, Minimax AI, int turn) {
         this.board = board;
         this.AI = AI;
-        this.user = user;
         this.turn = turn;
     }
-
-    public void firstMoveAI(){
-        board.addMove(board,1,10,10);
+    public int[] getBestMove(){
+        return AI.getBestMove(depth,1,false);
+    }
+    public int checkWinner(){
+        if(this.board.checkWin(1)) return 1;
+        if(this.board.checkWin(-1)) return -1;
+        return 0;
     }
 }
